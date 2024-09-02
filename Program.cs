addcad364faee614995eb5f0222e46425a72d4ee
Katelli <aska082024@gmail.com>
2024-09-02 10:58:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace c_sharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string datatype variabel
+        string helloWorld = "Hello, World!";
+        // heltall variabel
+        int myNumber = 10;
+        // desimaltall/dobbel variabel
+        double myDecimal = 10.25;
+        // bruker Console klassen til å skrive ut til terminalen
+        Console.WriteLine(helloWorld);
+        Console.WriteLine(myNumber);
+        Console.WriteLine(myDecimal);
+
+        // skriver ut en melding til brukeren
+        Console.WriteLine("Hei, hva heter du?");
+        // string variabel navn holder på verdien vi gir til konsollen
+        string? navn = Console.ReadLine();
+        // skriver ut en liten melding + navnet som er gitt til konsollen
+        Console.WriteLine("Hei " + navn);
+        // char typen, tar en bestemt bokstav
+        char a = '@';
+        Console.WriteLine(a);
+    }
+}
